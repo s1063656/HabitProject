6b06a65c3f771ea85d898ffbc05cffdf88128dc7
him993p <him993p@gmail.com>
2020-03-28 18:33:46 +0800

    M28_b

---
+++ app/src/main/java/tw/edu/pu/habit/Main2Activity.kt
@@ -0,0 +1,12 @@
+package tw.edu.pu.habit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
---
+++ app/src/main/java/tw/edu/pu/habit/MainActivity.kt
@@ -0,0 +1,10 @@
+package tw.edu.pu.habit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.l
