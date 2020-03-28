673a8b95a391de2ec6ebf9620e81d1ed6665685b
s1063656 <s1063656@gm.pu.edu.tw>
2020-03-28 22:24:36 +0800

    0328_a

--- app/src/main/java/tw/edu/pu/habit/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/habit/MainActivity.kt
@@ -7,4 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.l
+        setContentView(R.layout.activity_main)
+    }
+}
