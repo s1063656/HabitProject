99b6fd107b288dc20e8293f0cc226631988924ea
s1063656 <s1063656@gm.pu.edu.tw>
2020-04-01 02:16:32 +0800

    0401_b

--- app/src/main/java/tw/edu/pu/habit/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/habit/MainActivity.kt
@@ -1,6 +1,8 @@
 package tw.edu.pu.habit
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
@@ -12,5 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    public fun enter(v: View) {
+        val intent = Intent(this@MainActivity, Main2Activity::class.java)
+        startActivity(intent)
     }
 }
