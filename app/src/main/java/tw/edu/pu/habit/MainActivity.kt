0e17a564176ac3f6cc9640bdbaf796da87e1fd4d
s1063656 <s1063656@gm.pu.edu.tw>
2020-04-01 00:39:06 +0800

    0401_a

--- app/src/main/java/tw/edu/pu/habit/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/habit/MainActivity.kt
@@ -1,9 +1,13 @@
 package tw.edu.pu.habit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
